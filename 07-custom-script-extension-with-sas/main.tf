8d8aea86ca742909f8e6766c9e0480923f71002d
Ankita Shrivastava <ankitashrivastava@Ankitas-MacBook-Air.local>
2026-05-22 21:39:01 +0200

    Fix storage account suffix reference in custom script lab

--- 07-custom-script-extension-with-sas/main.tf
+++ 07-custom-script-extension-with-sas/main.tf
@@ -78,7 +78,7 @@ resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
 
 # Create an Azure Storage Account
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.var_storage_account_name}${random_string.unique_storage_acc.result}"
+  name                     = "${var.var_storage_account_name}${random_string.suffix_storage_acc.result}"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = var.var_location
   account_tier             = "Standard"
